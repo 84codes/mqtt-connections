5b4fa6aa70d25c7a9ee1cdcc4bc9b2b0939f526b
Carl Hörberg <carl.hoerberg@gmail.com>
2020-12-14 11:40:32 +0100

    can create multiple connections via args

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://guest:guest@localhost')
+const mqtt = require('mqtt')
 
-client.on('connect', function () {
-  client.subscribe('presence', function (err) {
-    if (!err) {
-      client.publish('presence', 'Hello mqtt')
-    }
+if (process.argv.length != 4) {
+  console.log("Usage: node app.js URI CONNECTIONS")
+  process.exit()
+}
+const uri = process.argv[2]
+const conns = parseInt(process.argv[3])
+
+for(let i = 0; i < conns; i++) {
+  const client = mqtt.connect('mqtt://guest:guest@localhost')
+
+  client.on('connect', function () {
+    client.subscribe('presence', function (err) {
+      if (!err) {
+        client.publish('presence', 'Hello mqtt')
+      }
+    })
   })
-})
 
-client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString())
-  client.end()
-})
+  client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString())
+    client.end()
+  })
+}
